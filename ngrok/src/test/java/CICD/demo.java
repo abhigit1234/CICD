aabd8d6e49521be8be3e4be4a5eaa43838bc1fee
abhilash <abhilashnaidu21@gmail.com>
2024-04-03 15:27:22 +0530

    second

--- ngrok/src/test/java/CICD/demo.java
+++ ngrok/src/test/java/CICD/demo.java
@@ -2,18 +2,19 @@ package CICD;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class demo {
 
-	
-	public static void main(String[] args) {
-	
-		for(int i=0;i<3;i++) {
-		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
-		driver.close();
+	@Test
+	public void test() {
+
+		for (int i = 0; i < 3; i++) {
+
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.google.com");
+			System.out.println(driver.getTitle());
+			driver.close();
 		}
-		}
+	}
 }
